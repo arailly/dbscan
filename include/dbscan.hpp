07e45f0a9a006839b455be6e6d85209831a76442
Yusuke Arai <yusuke.arailly@gmail.com>
2020-06-21 18:42:52 +0900

    :tada: complete dbscan with graph index

--- include/dbscan.hpp
+++ include/dbscan.hpp
@@ -28,6 +28,183 @@ namespace dbscan {
 
     using Cluster = vector<vector<int>>;
 
+    struct Node {
+        const Data<> data;
+        vector<int> neighbors;
+        unordered_map<size_t, bool> added;
+
+        void init() { added[data.id] = true; }
+        Node() : data(Data<>(0, {0})) { init(); }
+        Node(Data<>& p) : data(move(p)) { init(); }
+
+        void add_neighbor(int node_id) {
+            if (added.find(node_id) != added.end()) return;
+            added[node_id] = true;
+            neighbors.emplace_back(node_id);
+        }
+
+        void clear_neighbor() {
+            neighbors.clear();
+            added.clear();
+            added[data.id] = true;
+        }
+
+        auto get_n_neighbors() const { return neighbors.size(); }
+    };
+
+    struct GraphIndex {
+        vector<Node> nodes;
+
+        auto size() const { return nodes.size(); }
+
+        auto empty() const { return nodes.empty(); }
+
+        auto begin() const { return nodes.begin(); }
+
+        auto end() const { return nodes.end(); }
+
+        auto& operator[](size_t i) { return nodes[i]; }
+
+        auto& operator[](const Node &n) { return nodes[n.data.id]; }
+
+        const auto& operator[](size_t i) const { return nodes[i]; }
+
+        const auto& operator[](const Node &n) const { return nodes[n.data.id]; }
+
+        void init_data(Series<>& series) {
+            for (auto &point : series) {
+                nodes.emplace_back(point);
+            }
+        }
+
+        void make_bidirectional() {
+            for (auto &node : nodes) {
+                for (const auto neighbor_id : node.neighbors) {
+                    nodes[neighbor_id].add_neighbor(node.data.id);
+                }
+            }
+        }
+
+        void load(Series<>& series, const string& graph_path, int n, int degree = -1) {
+            init_data(series);
+
+            // csv file
+            if (is_csv(graph_path)) {
+                ifstream ifs(graph_path);
+                if (!ifs) {
+                    const string message = "Can't open file!: " + graph_path;
+                    throw runtime_error(message);
+                }
+
+                string line;
+                while (getline(ifs, line)) {
+                    const auto ids = split<size_t>(line);
+                    nodes[ids[0]].add_neighbor(ids[1]);
+                    if (degree != -1 && nodes[ids[0]].get_n_neighbors() >= degree) break;
+                }
+                return;
+            }
+
+            // dir
+#pragma omp parallel
+            {
+#pragma omp for schedule(dynamic, 10) nowait
+                for (int i = 0; i < n; i++) {
+                    const string path = graph_path + "/" + to_string(i) + ".csv";
+                    ifstream ifs(path);
+
+                    if (!ifs) {
+                        const string message = "Can't open file!: " + path;
+                        throw runtime_error(message);
+                    }
+
+                    string line;
+                    while (getline(ifs, line)) {
+                        const auto ids = split<size_t>(line);
+                        if (degree == -1 || nodes[ids[0]].get_n_neighbors() < degree) {
+                            nodes[ids[0]].add_neighbor(ids[1]);
+                        }
+                    }
+                }
+            };
+        }
+
+        void load(const string& data_path, const string& graph_path,
+                  int n = -1, int degree = -1) {
+            auto series = load_data(data_path, n);
+            load(series, graph_path, n, degree);
+        }
+
+        virtual void save(const string &save_path) {
+            // csv
+            if (is_csv(save_path)) {
+                ofstream ofs(save_path);
+                string line;
+                for (const auto &node : nodes) {
+                    line = to_string(node.data.id);
+                    for (const auto &neighbor_id : node.neighbors) {
+                        line += ',' + to_string(neighbor_id);
+                    }
+                    line += '\n';
+                    ofs << line;
+                }
+                return;
+            }
+
+            // dir
+            vector<string> lines(static_cast<unsigned long>(ceil(nodes.size() / 1000.0)));
+            for (const auto& node : nodes) {
+                const size_t line_i = node.data.id / 1000;
+                for (const auto& neighbor_id : node.neighbors) {
+                    lines[line_i] += to_string(node.data.id) + "," +
+                                     to_string(neighbor_id) + "\n";
+                }
+            }
+
+            for (int i = 0; i < lines.size(); i++) {
+                const string path = save_path + "/" + to_string(i) + ".csv";
+                ofstream ofs(path);
+                ofs << lines[i];
+            }
+        }
+
+        auto self_range_search(int query_id, float range) const {
+            const auto& query_node = nodes[query_id];
+
+            unordered_map<int, bool> added;
+            added[query_id] = true;
+
+            unordered_map<int, bool> checked;
+
+            vector<int> result;
+            result.emplace_back(query_id);
+
+            while (true) {
+                int first_unchecked_id = 0;
+                bool is_over = true;
+                for (const auto& e : result) {
+                    if (checked[e]) continue;
+                    checked[e] = true;
+                    first_unchecked_id = e;
+                    is_over = false;
+                    break;
+                }
+
+                if (is_over) return result;
+
+                const auto& first_unchecked_node = nodes[first_unchecked_id];
+                for (const auto& neighbor_id : first_unchecked_node.neighbors) {
+                    if (added[neighbor_id]) continue;
+                    added[neighbor_id] = true;
+
+                    const auto& neighbor = nodes[neighbor_id];
+                    const auto dist = euclidean_distance(query_node.data, neighbor.data);
+                    if (dist < range) result.emplace_back(neighbor_id);
+                }
+            }
+        }
+    };
+
     struct DBSCAN {
         double eps;
         int minpts;
@@ -79,14 +256,23 @@ namespace dbscan {
         }
 
         // cite from https://ja.wikipedia.org/wiki/DBSCAN
-        void fit(string data_path, int n = -1) {
-            database = convert(load_data(data_path, n));
+        void fit(const Dataset<>& dataset, const GraphIndex& aknng = GraphIndex()) {
+            database = convert(dataset);
 
             // calculate eps neighbors
             vector<vector<int>> eps_neighbors_list(database.size());
+
+            // use aknng if aknng is given
+            if (!aknng.empty()) {
 #pragma omp parallel for simd
-            for (int id = 0; id < database.size(); ++id)
-                eps_neighbors_list[id] = scan_eps_neighbors(id);
+                for (int id = 0; id < database.size(); ++id)
+                    eps_neighbors_list[id] = aknng.self_range_search(id, eps);
+            }
+            else {
+#pragma omp parallel for simd
+                for (int id = 0; id < database.size(); ++id)
+                    eps_neighbors_list[id] = scan_eps_neighbors(id);
+            }
 
             // clustering
             int cluster_id = -1;
@@ -106,6 +292,11 @@ namespace dbscan {
                                    eps_neighbors_list, visited);
                 }
             }
+        }
+
+        void fit(string data_path, int n = -1) {
+            const auto dataset = load_data(data_path, n);
+            fit(dataset);
         }
 
         void save(string save_path) {
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -35,3 +35,23 @@ TEST(dbscan, moon) {
 
     dbscan.save(save_path);
 }
+
+TEST(dbscan, small_with_graph) {
+    string base_dir = "/tmp/tmp.VzjXG3U2nU/test/src/";
+    string data_path = base_dir + "data1.csv";
+    string graph_path = base_dir + "graph1.csv";
+
+    GraphIndex graph;
+    graph.load(data_path, graph_path);
+
+    double eps = 2.5;
+    int minpts = 3;
+
+    auto dbscan = DBSCAN(eps, minpts);
+    dbscan.fit(data_path);
+
+    ASSERT_EQ(dbscan.cluster.size(), 2);
+    ASSERT_EQ(dbscan.database[0].cluster_id, 0);
+    ASSERT_EQ(dbscan.database[1].cluster_id, 1);
+    ASSERT_EQ(dbscan.database[10].cluster_id, -1);
+}
