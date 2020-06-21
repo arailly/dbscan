05b770ce49c6ba1abaea9291f63ea311ed4fdfe1
Yusuke Arai <yusuke.arailly@gmail.com>
2020-06-21 13:57:01 +0900

    :tada: complete dbscan with scan

--- include/dbscan.hpp
+++ include/dbscan.hpp
@@ -19,11 +19,9 @@ namespace dbscan {
 
     // arailib::Dataset -> dbscan::Database
     auto convert(Dataset<> dataset) {
-        Database result(dataset.size());
-#pragma omp parallel for
-        for (int id = 0; id < dataset.size(); ++id) {
-            const auto& data = dataset[id];
-            result[id] = Point(data);
+        Database result;
+        for (const auto& data : dataset) {
+            result.emplace_back(Point(data));
         }
         return result;
     }
@@ -42,6 +40,8 @@ namespace dbscan {
             const auto& query = database[point_id];
             vector<int> result;
             for (const auto data : database) {
+                if (point_id == data.id) continue;
+
                 const auto dist = euclidean_distance(query, data);
                 if (dist < eps)
                     result.emplace_back(data.id);
@@ -49,6 +49,36 @@ namespace dbscan {
             return result;
         }
 
+        void assign(Point& point, int cluster_id) {
+            point.cluster_id = cluster_id;
+            cluster[cluster_id].emplace_back(point.id);
+        }
+
+        void expand_cluster(Point& point, vector<int> neighbors, int cluster_id,
+                            const vector<vector<int>>& eps_neighbors_list,
+                            unordered_map<int, bool>& visited) {
+            // assign cluster
+            assign(point, cluster_id);
+
+            for (int i = 0; i < neighbors.size(); ++i) {
+                const auto neighbor_id = neighbors[i];
+                auto& neighbor = database[neighbor_id];
+                if (!visited[neighbor_id]) {
+                    visited[neighbor_id] = true;
+
+                    const auto& neighbor_neighbors = eps_neighbors_list[neighbor_id];
+                    if (neighbor_neighbors.size() >= minpts) {
+                        for (const auto neighbor_neighbor_id : neighbor_neighbors)
+                            neighbors.emplace_back(neighbor_neighbor_id);
+                    }
+                }
+                if (neighbor.cluster_id < 0) {
+                    assign(neighbor, cluster_id);
+                }
+            }
+        }
+
+        // cite from https://ja.wikipedia.org/wiki/DBSCAN
         void fit(string data_path, int n = -1) {
             database = convert(load_data(data_path, n));
 
@@ -58,8 +88,8 @@ namespace dbscan {
             for (int id = 0; id < database.size(); ++id)
                 eps_neighbors_list[id] = scan_eps_neighbors(id);
 
-            // assign cluster
-            int cluster_id = 0;
+            // clustering
+            int cluster_id = -1;
             unordered_map<int, bool> visited;
 
             for (auto& point : database) {
@@ -71,7 +101,18 @@ namespace dbscan {
                     point.cluster_id = -1; // noise
                 else {
                     ++cluster_id;
+                    cluster.resize(cluster_id + 1);
+                    expand_cluster(point, eps_neighbors, cluster_id,
+                                   eps_neighbors_list, visited);
                 }
+            }
+        }
+
+        void save(string save_path) {
+            ofstream ofs(save_path);
+            ofs << "cluster_id" << endl;
+            for (const auto& point : database) {
+                ofs << point.cluster_id << endl;
             }
         }
     };
--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,3 +1,37 @@
 #include <gtest/gtest.h>
 #include <arailib.hpp>
+#include <dbscan.hpp>
 
+using namespace arailib;
+using namespace dbscan;
+
+TEST(dbscan, small) {
+    string data_path = "/tmp/tmp.VzjXG3U2nU/test/src/data1.csv";
+    double eps = 1.5;
+    int minpts = 2;
+
+    auto dbscan = DBSCAN(eps, minpts);
+    dbscan.fit(data_path);
+
+    ASSERT_EQ(dbscan.cluster.size(), 2);
+    ASSERT_EQ(dbscan.database[0].cluster_id, 0);
+    ASSERT_EQ(dbscan.database[1].cluster_id, 1);
+    ASSERT_EQ(dbscan.database[10].cluster_id, -1);
+}
+
+TEST(dbscan, moon) {
+    string data_path = "/home/arai/workspace/dataset/cluster/moon.csv";
+    string save_path = "/home/arai/workspace/result/clustering/dbscan/moon.csv";
+
+    double eps = 0.2;
+    int minpts = 5;
+
+    auto dbscan = DBSCAN(eps, minpts);
+    dbscan.fit(data_path);
+
+    ASSERT_EQ(dbscan.cluster.size(), 2);
+    ASSERT_EQ(dbscan.cluster[0].size(), 100);
+    ASSERT_EQ(dbscan.cluster[1].size(), 99);
+
+    dbscan.save(save_path);
+}
